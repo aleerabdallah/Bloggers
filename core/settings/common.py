6feee7b297ddc6ea9a8fb156c67bef53ea223dda
aleerabdallah <juniorab444@gmail.com>
2024-08-29 17:34:45 +0200

    Added render host to ALLOWED_HOST

--- core/settings/common.py
+++ core/settings/common.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "bloggers-xp1a.onrender.com"]
 
 
 sys.path.insert(0, str(BASE_DIR / 'apps'))
